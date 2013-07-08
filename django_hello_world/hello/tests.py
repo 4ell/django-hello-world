34d70c119278a703eba4bc399a6146f66ed2be6a
4ell <4ell@xakep.ru>
2013-07-08 23:56:39 +0300

    ticket:3 Fix tests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from json import dumps, loads
+from json import loads
 from models import Person, ReqData
 from datetime import date, datetime
 
@@ -10,7 +10,7 @@ class HttpTest(TestCase):
         params = ['name', 'last_name', 'birthday', 'email',
                   'jabber', 'skype', 'bio', 'contacts']
 
-        person = Person.objects.create(
+        Person.objects.create(
             name='Steve',
             last_name='Jobs',
             birthday=date(1955, 2, 24),
@@ -29,7 +29,7 @@ class HttpTest(TestCase):
         self.assertContains(response, '42 Coffee Cups Test Assignment')
 
 
-class MiddlewareTest(TestCase):    
+class MiddlewareTest(TestCase):
     def test_create_obj(self):
         ReqData.objects.create(
             time=datetime.now(),
@@ -41,21 +41,31 @@ class MiddlewareTest(TestCase):
 
     def test_do_requests(self):
         self.client.get('/')
-        
+
         for i in range(5):
             self.client.get('/?{0}={1}'.format(i, i**2))
-        
-        self.client.login('admin', 'admin')
+
+        self.client.login()
 
         for i in range(5):
-            self.client.post('/', {a:i, b:i**2})
+            self.client.post('/', {i: i**2})
 
-    def test_read_db(self):
-        requests = ReqData.objects.all()[-11:]
+        requests = ReqData.objects.all().reverse()[:11]
+        self.assertTrue(requests.exists())
         for req in requests:
             get = loads(req.get)
             post = loads(req.post)
-            for k, v in get.items():
-                assertEqual(k**2, v)
-            for k, v in post.items():
-                assertEqual(k**2, v)
+            for i in get.items():
+                k, v = map(int, i)
+                self.assertEqual(k**2, v)
+            for i in post.items():
+                k, v = map(int, i)
+                self.assertEqual(k**2, v)
+
+    def test_views(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'requests')
+
+        response = self.client.get('/requests/')
+        self.assertEqual(response.status_code, 200)
