8d7f4b8538c560bb65c3c5a28b950817a9c9a645
4ell <4ell@xakep.ru>
2013-07-09 02:02:06 +0300

    ticket:5 Edit page

--- django_hello_world/urls.py
+++ django_hello_world/urls.py
@@ -8,14 +8,16 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'django_hello_world.hello.views.bio', name='bio'),
     # url(r'^django_hello_world/', include('django_hello_world.foo.urls')),
+    url(r'^edit/', 'django_hello_world.hello.views.edit', name='edit'),
 
     url(r'^requests/', 'django_hello_world.hello.views.requests', name='requests'),
-    
+
     url(r'^login/', 'django.contrib.auth.views.login', {
         'template_name': 'hello/login.html', 
         'extra_context': {'next': '/'}
     }),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
