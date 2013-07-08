d04e381188486986be642cbaa1eac763e3acd605
4ell <4ell@xakep.ru>
2013-07-08 23:14:29 +0300

    ticket:1 Tests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -1,27 +1,30 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from models import Person
+from datetime import date
 
 
 class HttpTest(TestCase):
-    def test_home(self):
-        c = Client()
-        response = c.get(reverse('home'))
+    def test_index(self):
+        params = ['name', 'last_name', 'birthday', 'email',
+                  'jabber', 'skype', 'bio', 'contacts']
+
+        person = Person.objects.create(
+            name='Steve',
+            last_name='Jobs',
+            birthday=date(1955, 2, 24),
+            email='steve@apple.com',
+            jabber='steve@apple.im',
+            skype='steve_jobs',
+            bio='Some information',
+            contacts='Some information'
+        )
+
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello!')
+        self.assertTrue('person' in response.context)
+        for param in params:
+            self.assertTrue(hasattr(response.context['person'], param))
+        self.assertContains(response, '42 Coffee Cups Test Assignment')
