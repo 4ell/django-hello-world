0749b713f9ee9d56792439eabf16b085b0d2aea6
4ell <4ell@xakep.ru>
2013-07-08 23:31:06 +0300

    ticket:1 Working version

--- django_hello_world/hello/models.py
+++ django_hello_world/hello/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthday = models.DateField()
+    email = models.EmailField()
+    jabber = models.CharField(max_length=30)
+    skype = models.CharField(max_length=30)
+    bio = models.TextField()
+    contacts = models.TextField()
--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,6 +1,14 @@
 from annoying.decorators import render_to
 from django.contrib.auth.models import User
 
+from models import Person
+
+
+@render_to('hello/bio.html')
+def bio(request):
+    person = Person.objects.latest('id')
+    return {'person': person}
+
 
 @render_to('hello/home.html')
 def home(request):
