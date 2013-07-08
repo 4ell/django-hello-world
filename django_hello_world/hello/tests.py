52ea0bd248c2adeb35fd444d058152cbef160bb0
4ell <4ell@xakep.ru>
2013-07-08 23:55:23 +0300

    ticket:3 Tests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
-from models import Person
-from datetime import date
+from json import dumps, loads
+from models import Person, ReqData
+from datetime import date, datetime
 
 
 class HttpTest(TestCase):
@@ -9,7 +10,7 @@ class HttpTest(TestCase):
         params = ['name', 'last_name', 'birthday', 'email',
                   'jabber', 'skype', 'bio', 'contacts']
 
-        Person.objects.create(
+        person = Person.objects.create(
             name='Steve',
             last_name='Jobs',
             birthday=date(1955, 2, 24),
@@ -26,3 +27,35 @@ class HttpTest(TestCase):
         for param in params:
             self.assertTrue(hasattr(response.context['person'], param))
         self.assertContains(response, '42 Coffee Cups Test Assignment')
+
+
+class MiddlewareTest(TestCase):    
+    def test_create_obj(self):
+        ReqData.objects.create(
+            time=datetime.now(),
+            path='/',
+            get='{}',
+            post='{}',
+            cookies='{}'
+        )
+
+    def test_do_requests(self):
+        self.client.get('/')
+        
+        for i in range(5):
+            self.client.get('/?{0}={1}'.format(i, i**2))
+        
+        self.client.login('admin', 'admin')
+
+        for i in range(5):
+            self.client.post('/', {a:i, b:i**2})
+
+    def test_read_db(self):
+        requests = ReqData.objects.all()[-11:]
+        for req in requests:
+            get = loads(req.get)
+            post = loads(req.post)
+            for k, v in get.items():
+                assertEqual(k**2, v)
+            for k, v in post.items():
+                assertEqual(k**2, v)
