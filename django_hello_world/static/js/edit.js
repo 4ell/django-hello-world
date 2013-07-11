02d9c979b5aabfbd3c4dcb60a7100f6685b93245
4ell <4ell@xakep.ru>
2013-07-11 15:06:17 +0300

    ticket:6 Edit templates

--- django_hello_world/static/js/edit.js
+++ django_hello_world/static/js/edit.js
@@ -1,3 +1,4 @@
+statout = null
 
 function qs() {
 	return document.querySelector.apply(document, arguments)
@@ -5,13 +6,50 @@ function qs() {
 
 function show_photo() {
 	var input = qs('#id_photo')
-    if (input.files && input.files[0]) {
-        var reader = new FileReader()
+	if (input.files && input.files[0]) {
+		var reader = new FileReader()
 
-        reader.onload = function (e) {
-            qs('img.photo').src = e.target.result
-        }
+		reader.onload = function (e) {
+			qs('img.photo').src = e.target.result
+		}
 
-        reader.readAsDataURL(input.files[0])
-    }
+		reader.readAsDataURL(input.files[0])
+	}
 }
+
+function show_status(msg, color, timeout) {
+	color = color || ''
+	timeout = timeout || 15
+	var code = '<div class="status '+color+'">'+msg+'</div>'
+
+	$('#menu .status').remove()
+	$('#menu').append(code)
+
+	window.clearTimeout(statout)
+	statout = window.setTimeout(function() {
+		$('#menu .status').remove()
+	}, timeout * 1000)
+}
+
+function show_errors(errors) {
+	// body...
+	console.log(errors)
+}
+
+$(document).ready(function() {
+	// bind 'myForm' and provide a simple callback function 
+	$('form#bio').ajaxForm({
+		beforeSubmit: function() {
+			show_status('Sending...')
+		},
+		success: function(response) {
+			if(response.saved) {
+				show_status('Saved', 'success')
+			} else {
+				show_status('Error', 'error')
+				show_errors(response.errors)
+			}
+		},
+		timeout: 1500
+	})
+})
