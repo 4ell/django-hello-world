7e0750373ca0b5b0ff8c617bae393c5b243afa4c
4ell <4ell@xakep.ru>
2013-07-16 19:42:20 +0300

    ticket:14 pep8

--- django_hello_world/hello/urls.py
+++ django_hello_world/hello/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls.defaults import patterns, url
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', 'django_hello_world.hello.views.bio', name='bio'),
-    
-    url(r'^edit/save/', 'django_hello_world.hello.views.save_person', name='save_person'),
+
+    url(r'^edit/save/', 'django_hello_world.hello.views.save_person',
+        name='save_person'),
     url(r'^edit/', 'django_hello_world.hello.views.edit', name='edit'),
 
-    url(r'^requests/(asc|desc)?/?', 'django_hello_world.hello.views.requests', name='requests'),
+    url(r'^requests/(asc|desc)?/?', 'django_hello_world.hello.views.requests',
+        name='requests'),
 
     url(r'^login/', 'django.contrib.auth.views.login', {
-        'template_name': 'hello/login.html', 
+        'template_name': 'hello/login.html',
         'extra_context': {'next': '/'}
-    }),
+        }),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
